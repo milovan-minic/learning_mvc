22a258c8dbc6ada7c8b7baa3520a1b7f919e0474
milovan-minic <milovan.minic@gmail.com>
2016-06-12 17:39:03 +0200

    Adds index.php file

---
+++ public/index.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ *  Front controller
+ *
+ *  PHP version 5.5.9
+ */
+
+//echo 'Requested URL = "' . $_SERVER['QUERY_STRING'] . '"';
+
+/**
+ *  Routing
+ */
+require '../Core/Router.php';
+
+$router = new Router();
+
+//echo get_class($router);
+
+// Add the routes
+$router->add('', ['controller' => 'Home', 'action' => 'index']);
+$router->add('posts', ['controller' => 'Posts', 'action' => 'index']);
+//$router->add('posts/new', ['controller' => 'Posts', 'action' => 'new']);
+$router->add('{controller}/{action}');
+$router->add('admin/{action}/{controller}');
+
+// Display the routing table
+//echo '<pre>';
+var_dump($router->getRoutes());
+//echo '</pre>';
+
+// Match the requested route
+$url = $_SERVER['QUERY_STRING'];
+
+if($router->match($url)) {
+    var_dump($router->getParams());
+} else {
+    echo 'No route found for URL "' . $url . '"';
+}
