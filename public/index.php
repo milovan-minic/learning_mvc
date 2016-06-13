20c1cf610c7c05815f3f80ed7957ec651105c366
milovan-minic <milovan.minic@gmail.com>
2016-06-13 12:44:32 +0200

    Add the route to the front controller

--- public/index.php
+++ public/index.php
@@ -22,11 +22,11 @@ $router->add('', ['controller' => 'Home', 'action' => 'index']);
 $router->add('posts', ['controller' => 'Posts', 'action' => 'index']);
 //$router->add('posts/new', ['controller' => 'Posts', 'action' => 'new']);
 $router->add('{controller}/{action}');
-$router->add('admin/{action}/{controller}');
+$router->add('{controller}/{id:\d+}/{action}');
 
 // Display the routing table
 //echo '<pre>';
-var_dump($router->getRoutes());
+//var_dump($router->getRoutes());
 //echo '</pre>';
 
 // Match the requested route
