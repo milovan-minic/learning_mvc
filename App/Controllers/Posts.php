2f04a1fd3bc33af02c17ccfdbf4e94f2231623ba
milovan-minic <milovan.minic@gmail.com>
2016-06-13 20:09:38 +0200

    Adds Posts controller, initial version of it

---
+++ App/Controllers/Posts.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ *  Posts Controller
+ *
+ *  PHP Version 5.5.9
+ */
+
+class Posts {
+
+    /**
+     *  Show the index page
+     *
+     * @return void
+     */
+    public function index()
+    {
+        echo 'Hello from index action in the Posts Controller!';
+    }
+
+    /**
+     *  Show the add new page
+     *
+     * @return void
+     */
+    public function addNew()
+    {
+        echo 'Hello from addNew action in the Posts Controller!';
+    }
+}
