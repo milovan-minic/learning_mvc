c20ef53a8cdba7fc97d46efaef4857ab5525a221
milovan-minic <milovan.minic@gmail.com>
2016-06-14 09:53:50 +0200

    Adds Home controllerr

---
+++ App/Controllers/Home.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controllers;
+
+/**
+ *  Home Controller
+ *
+ *  PHP Version 5.5.9
+ */
+
+class Home
+{
+    /**
+     *  Show the index page
+     *
+     * @return void
+     */
+    public function index()
+    {
+        echo 'Hello from the index action in the Home controller';
+    }
+}
