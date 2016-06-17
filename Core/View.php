10919df9880f3d2511fedf2e8428fc6ae9df37cd
milovan-minic <milovan.minic@gmail.com>
2016-06-17 14:03:25 +0200

    Adds render view method in Core library

---
+++ Core/View.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Core;
+
+/**
+ *  View
+ *
+ *  PHP Version 5.5.9
+ */
+
+class View
+{
+    /**
+     *  Render a view file
+     *
+     * @param string $view The view file
+     *
+     * @return void
+     */
+    public static function render($view)
+    {
+        $file = "../App/Views/$view"; // Relative to Core directory
+
+        if(is_readable($file)){
+            require $file;
+        } else {
+            echo "$file not found";
+        }
+    }
+}
