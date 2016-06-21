2ae010e7876b6e1ab478078786c6cb3455f964b4
milovan-minic <milovan.minic@gmail.com>
2016-06-21 14:59:55 +0200

    Put application configuration in a separate configuration file

---
+++ App/Config.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App;
+
+/**
+ *  Application configuration
+ *
+ *  PHP Version 5.5.9
+ */
+
+/**
+ * Class Config
+ * @package App
+ */
+class Config
+{
+    /**
+     *  Database host
+     * @var string
+     */
+    const DB_HOST = 'localhost';
+
+    /**
+     *  Database name
+     * @var string
+     */
+    const DB_NAME = 'learning_mvc';
+
+    /**
+     *  Database username
+     * @var string
+     */
+    const DB_USERNAME = 'root';
+
+    /**
+     *  Database password
+     * @var string
+     */
+    const DB_PASSWORD = 'misa';
+}
--- Core/Model.php
+++ Core/Model.php
@@ -4,6 +4,7 @@ namespace Core;
 
 use PDO;
 use PDOException;
+use \App\Config;
 
 /**
  *  Base model
@@ -22,16 +23,23 @@ abstract class Model
         static $db = null;
 
         if($db === null) {
-            $host = 'localhost';
-            $dbName = 'learning_mvc';
-            $username = 'root';
-            $password = 'misa';
+//            $host = 'localhost';
+//            $dbName = 'learning_mvc';
+//            $username = 'root';
+//            $password = 'misa';
 
             try{
-                $db = new PDO("mysql:host=$host;dbname=$dbName;charset=utf8",
-                                $username, $password);
+//                $db = new PDO("mysql:host=$host;dbname=$dbName;charset=utf8",
+//                                $username, $password);
+
+                $dsn =  'mysql:host=' . Config::DB_HOST .
+                        ';dbname=' . Config::DB_NAME .
+                        ';charset=utf8';
+
+                $db = new PDO($dsn, Config::DB_USERNAME, Config::DB_PASSWORD);
 
                 return $db;
+
             } catch(PDOException $e){
                 echo $e->getMessage();
             }
